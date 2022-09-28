906f9a77f4ba43ce3b6d3f2feee9eb33834cd0cf
tariklovic <114573309+tariklovic@users.noreply.github.com>
2022-09-28 11:26:12 +0200

    Add files via upload

---
+++ vjezba-3.py
@@ -0,0 +1,8 @@
+#Varijable - vježba 2, input i output
+ime = input("Unesi svoje ime:")
+print("Pozdrav, + ime")
+
+a = int(input("Unesi broj a: ")) # koristimo int()
+b = int(input("Unesi broj b:"))
+zbroj = a + b
+print("Zbroj brojeva {0} i {1} iznosi {2} ".format(a, b, zbroj))
